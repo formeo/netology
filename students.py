6e7455ce72d600b5ec379f846b3672a71d868a71
Gordienko Roman <formeo@yandex.ru>
2017-08-06 22:14:53 +0500

    Create students.py

---
+++ students.py
@@ -0,0 +1,15 @@
+# home work for 1.5 lessons
+students = [
+ 
+  {"name":"Ivan", "surname":"Ivanov", "sex":"male", "experienced":1, "homework": [6, 8, 10, 10, 8], "exam":8},
+  
+  {"name":"Peter", "surname":"Petrov", "sex":"male", "experienced":0, "homework": [4, 7, 9, 3, 6], "exam":6},
+  
+  {"name":"Alice", "surname":"Ivanova", "sex":"female", "experienced":1, "homework": [9, 10, 10, 9, 8], "exam":9},
+  
+  {"name":"John", "surname":"Makarov", "sex":"male", "experienced":1, "homework": [2, 6, 4, 5, 9], "exam":4},
+  
+  {"name":"Pavel", "surname":"Pavlov", "sex":"male", "experienced":0, "homework": [9, 10, 10, 9, 8], "exam":9},
+  
+  {"name":"Irina", "surname":"Lebedeva", "sex":"female", "experienced":1, "homework": [8, 6, 10, 4, 5], "exam":2}
+]
