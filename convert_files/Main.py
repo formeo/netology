e2db2163c3edf6cda0d408402c569ffc552f527f
Gordienko Roman <formeo@yandex.ru>
2017-08-25 11:54:23 +0500

    Update Main.py

--- convert_files/Main.py
+++ convert_files/Main.py
@@ -2,17 +2,15 @@ import os
 import multiprocessing
 import subprocess
 
-
 def convert_file(filename):
     print('Идет конвертация файла',filename+".jpg")
-    subprocess.run(["convert","Source/"+filename+".jpg", "-resize","200","Result/"+filename+"_output.jpg"])
+    subprocess.run(["convert","Source/" + filename+".jpg", "-resize","200","Result/" + filename + "_output.jpg"])
 
 def del_files_in_directory(dirname):
        file_list = os.listdir(dirname)
        for file in file_list:
-           path = os.path.join(os.path.abspath(os.path.dirname(__file__)), dirname+'/'+file)
+           path = os.path.join(os.path.abspath(os.path.dirname(__file__)), dirname + '/' + file)
            os.remove(path)
-
 
 def create_folder(dirname):
     if not os.path.exists(dirname):
@@ -22,7 +20,7 @@ def create_folder(dirname):
 def get_source_files(dirname):
     files = os.listdir(dirname)
     images = filter(lambda x: x.endswith('.jpg'), files)
-    files_without_ext=[]
+    files_without_ext = []
     for  file_name in images:
         files_without_ext.append(file_name[:-4])
     return files_without_ext
@@ -32,12 +30,10 @@ def main_fuction():
     print('Вас приветствует программа конвертации файлов')
     del_files_in_directory('Result')
     create_folder('Result')
-    file_list=get_source_files('Source')
+    file_list = get_source_files('Source')
     p = multiprocessing.Pool(4)
     p.map(convert_file, file_list)
     print('Все файлы переконвертированны, спасибо')
-
-
 
 if __name__ == "__main__":
     main_fuction()
