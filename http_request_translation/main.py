e1afc0910ebee0b7f86fe4397ad26c415d34d6ac
Formeo <formeo@yandex.ru>
2017-09-11 22:17:45 +0500

    fix request bug

--- http_request_translation/main.py
+++ http_request_translation/main.py
@@ -6,19 +6,22 @@ URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
 KEY = 'trnsl.1.1.20161025T233221Z.47834a66fd7895d0.a95fd4bfde5c1794fa433453956bd261eae80152'
 OUT_DIR = 'translated'
 
+def translate(language,text):
+    params = {
+        'key': KEY,
+        'lang': language,
+        'text': text,
+    }
+    response = requests.get(URL, params=params).json()
+    return ' '.join(response.get('text', []))
+
 def translate_it(path_to_file_in, path_to_file_result, lang_from, lang_to = 'ru'):
     lang_from = lang_from.lower()
     language = lang_from + '-' + lang_to
     with open(path_to_file_result, 'tw', encoding = 'utf-8') as f_out:
         with open(path_to_file_in) as f_in:
             for line in f_in:
-                params = {
-                    'key': KEY,
-                    'lang': language,
-                    'text': line,
-                }
-                response = requests.get(URL, params=params).json()
-                f_out.writelines(' '.join(response.get('text', [])))
+                f_out.writelines(translate(language,line))
 
 
 def get_file_list():
@@ -37,8 +40,6 @@ def main_function():
     for item in files:
         with open(item) as f:
             file_out = os.path.join(OUT_DIR, item[:-4]+'-RU.txt')
-            if os.path.exists(file_out):
-                os.remove(file_out)
             translate_it(item, file_out, item[:-4], 'ru')
     print('Translated complete')
 
