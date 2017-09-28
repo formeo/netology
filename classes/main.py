797eeb901fe6df871d396350c1fa7e46713d3d5a
Formeo <formeo@yandex.ru>
2017-09-28 21:51:11 +0500

    fix names

--- classes/main.py
+++ classes/main.py
@@ -7,7 +7,7 @@
 # Должен быть один базовый класс, который наследуют все остальные животные.
 # Базовый класс должен определять общие характеристики и интерфейс.
 
-class Farm_Animal(object):
+class FarmAnimal(object):
     def __init__(
             self,
             species,
@@ -18,20 +18,20 @@ class Farm_Animal(object):
         self.resource = resource
         self.sound = sound
 
-    def getSpecies(self):
+    def get_species(self):
         return self.species
 
-    def getResource(self):
+    def get_resource(self):
         return self.resource
 
-    def getSound(self):
+    def get_sound(self):
         return self.sound
 
     def __str__(self):
         return 'it is a %s' % self.species
 
 
-class Farm_Animal_Bird(Farm_Animal):
+class FarmAnimalBird(FarmAnimal):
     def __init__(
             self,
             species,
@@ -39,39 +39,36 @@ class Farm_Animal_Bird(Farm_Animal):
             sound,
             canfly,
     ):
-        Farm_Animal.__init__(self, species, resource, sound)
+        super().__init__(species, resource, sound)
         self.canfly = canfly
 
-    def canFly(self):
-        if self.canfly:
-            return 'it is ready to fly'
-        else:
-            return 'No fly today'
+    def can_fly(self):
+        return self.canfly and 'it is ready to fly' or 'No fly today'
 
 
-class Cow(Farm_Animal):
+class Cow(FarmAnimal):
     def __init__(
             self,
             name,
             weight,
             color,
     ):
-        Farm_Animal.__init__(self, 'Cow', 'Milk', 'Mooo')
+        super().__init__('Cow', 'Milk', 'Mooo')
         self.weight = weight
         self.name = name
         self.color = color
 
-    def cowName(self):
+    def cow_name(self):
         return self.name
 
-    def cowWeight(self):
+    def cow_weight(self):
         return self.weight
 
-    def cowColor(self):
+    def cow_color(self):
         return '%s is %s color' % (self.name, self.color)
 
 
-class Goat(Farm_Animal):
+class Goat(FarmAnimal):
     def __init__(
             self,
             name,
@@ -79,26 +76,26 @@ class Goat(Farm_Animal):
             color,
             children,
     ):
-        Farm_Animal.__init__(self, 'Goat', 'Milk', 'Meee')
+        super().__init__('Goat', 'Milk', 'Meee')
         self.weight = weight
         self.name = name
         self.color = color
         self.children = children
 
-    def goatName(self):
+    def goat_name(self):
         return self.name
 
-    def goatWeight(self):
+    def goat_weight(self):
         return self.weight
 
-    def goatColor(self):
+    def goat_color(self):
         return '%s is %s color' % (self.name, self.color)
 
-    def goatHowManyChildren(self):
+    def goat_how_many_children(self):
         return '%s has %s children' % (self.name, self.children)
 
 
-class Sheep(Farm_Animal):
+class Sheep(FarmAnimal):
     def __init__(
             self,
             name,
@@ -106,101 +103,96 @@ class Sheep(Farm_Animal):
             color,
             wool,
     ):
-        Farm_Animal.__init__(self, 'Sheep', 'Wool', 'Beee')
+        super().__init__('Sheep', 'Wool', 'Beee')
         self.weight = weight
         self.name = name
         self.color = color
         self.wool = weight
 
-    def sheepName(self):
+    def sheep_name(self):
         return self.name
 
-    def sheepWeight(self):
+    def sheep_weight(self):
         return self.weight
 
-    def sheepColor(self):
+    def sheep_color(self):
         return '%s is %s color' % (self.name, self.color)
 
-    def sheepItHasWool(self):
-        if self.wool:
-            return '%s has wool' % self.name
-        else:
-            return '%s already was cropped' % self.name
+    def sheep_it_has_wool(self):
+        return self.wool and '%s has wool' % self.name or '%s already was cropped' % self.name
 
-
-class Pig(Farm_Animal):
+class Pig(FarmAnimal):
     def __init__(self, name, weight):
-        Farm_Animal.__init__(self, 'Pig', 'Meat', 'Oink')
+        super().__init__('Pig', 'Meat', 'Oink')
         self.weight = weight
         self.name = name
 
-    def pigName(self):
+    def pig_name(self):
         return self.name
 
-    def pigWeight(self):
+    def pig_weight(self):
         return self.weight
 
 
-class Duck(Farm_Animal_Bird):
+class Duck(FarmAnimalBird):
     def __init__(
             self,
             name,
             sex,
             ready_to_soup,
     ):
-        Farm_Animal_Bird.__init__(self, 'Duck', 'Meat', 'Quack', True)
+        super().__init__('Duck', 'Meat', 'Quack', True)
         self.sex = sex
         self.name = name
         self.ready_to_soup = ready_to_soup
 
-    def duckName(self):
+    def duck_name(self):
         return self.name
 
-    def duckSex(self):
+    def duck_sex(self):
         if self.sex == 'Male':
             return 'It is a drake'
         else:
             return 'It is a female duck'
 
-    def duckReady_to_soup(self):
+    def duck_ready_to_soup(self):
         if self.ready_to_soup:
             return 'Get ready to meal'
         else:
             return 'Just only couple of day'
 
 
-class Chicken(Farm_Animal_Bird):
+class Chicken(FarmAnimalBird):
     def __init__(self, name, eggs):
-        Farm_Animal_Bird.__init__(self, 'Chicken', 'Egg', 'KoKoKo',
-                                  False)
+        super().__init__('Chicken', 'Egg', 'KoKoKo',False)
         self.eggs = eggs
         self.name = name
 
-    def chickenName(self):
+    def chicken_name(self):
         return self.name
 
-    def chickenEggs(self):
+    def chicken_eggs(self):
         return self.eggs
 
 
-class Goose(Farm_Animal_Bird):
+class Goose(FarmAnimalBird):
     def __init__(self, name, color):
-        Farm_Animal_Bird.__init__(self, 'Goose', 'Meat', 'GaGa', True)
+        super().__init__('Goose', 'Meat', 'GaGa', True)
         self.color = color
         self.name = name
 
-    def gooseName(self):
+    def goose_name(self):
         return self.name
 
-    def gooseColor(self):
+    def goose_color(self):
         return self.color
 
 
 Peppa = Pig('Peppa', 300)
-print(Peppa.pigName())
-print(Peppa.getSound())
+print(Peppa.pig_name())
+print(Peppa.get_sound())
 
 Howard = Duck('Howard', 'Male', True)
-print(Howard.canFly())
-print(Howard.duckReady_to_soup())
-print(Howard.duckSex())
+print(Howard.can_fly())
+print(Howard.duck_ready_to_soup())
+print(Howard.duck_sex())
--- yandex_home_work/main.py
+++ yandex_home_work/main.py
@@ -18,65 +18,64 @@ APP_TOKEN='AQAAAAAAQlimAASOrV5mI1QOpUgilN_kPc2jySQ'
 
 class GetYandexMetrika:
 
-    management_url = 'https://api-metrika.yandex.ru/management/v1/counters'
-    stat_url = 'https://api-metrika.yandex.ru/stat/v1/data'
+    MANAGEMENT_URL = 'https://api-metrika.yandex.ru/management/v1/counters'
+    STAT_URL = 'https://api-metrika.yandex.ru/stat/v1/data'
 
     def __init__(self,token):
         self.token = token
 
-    def getHeaders(self):
+    def get_headers(self):
          return {
              'Content-Type' : 'application/json',
              'Authorization' : 'OAuth {}'.format(self.token)
          }
 
-    def GetCounters(self):
-        headers = self.getHeaders()
-        response = requests.get(self.management_url, headers=headers)
+    def get_counters(self):
+        headers = self.get_headers()
+        response = requests.get(self.MANAGEMENT_URL, headers=headers)
         return response.json()['counters']
 
-    def GetData(self,params_in):
-        headers = self.getHeaders()
+    def get_data(self,params_in):
+        headers = self.get_headers()
         params = params_in
-        response = requests.get(self.stat_url, params, headers=headers)
+        response = requests.get(self.STAT_URL, params, headers=headers)
         if response.json()['data'] == []:
             return 0
         else:
             return response.json()['data'][0]['metrics'][0]
 
-
-    def GetVisits(self,counter_id):
+    def get_visits(self,counter_id):
          params = {
              'id' : counter_id,
              'metrics' : 'ym:s:visits'
          }
-         return self.GetData(params)
+         return self.get_data(params)
 
-    def GetVievs(self,counter_id):
+    def get_views(self,counter_id):
         params = {
             'id' : counter_id,
             'metrics' : 'ym:s:pageviews'
         }
-        return self.GetData(params)
+        return self.get_data(params)
 
-    def GetVisitors(self,counter_id):
+    def get_visitors(self,counter_id):
         params = {
         'id': counter_id,
         'metrics' : 'ym:s:users'
         }
-        return self.GetData(params)
+        return self.get_data(params)
 
-    def GetAllData(self):
-        counters = self.GetCounters()
+    def get_all_data(self):
+        counters = self.get_counters()
         visits = 0
         views = 0
         users = 0
         for counter in counters:
-            visits += self.GetVisits(counter['id'])
-            views += self.GetVievs(counter['id'])
-            users += self.GetVisitors(counter['id'])
+            visits += self.get_visits(counter['id'])
+            views += self.get_views(counter['id'])
+            users += self.get_visitors(counter['id'])
         print('Всего визитов: {0}, просмотров: {1}, поситетелей: {2} '.format(visits, views, users))
 
 
 metrika = GetYandexMetrika(APP_TOKEN)
-metrika.GetAllData()
+metrika.get_all_data()
